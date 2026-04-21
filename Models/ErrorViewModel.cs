d708445615ea2b7f6b658f0d22e4c3de839aa2a2
LarsAtSchool <031688@student.usn.no>
2026-04-21 13:23:53 +0200

    Commit 1 - Oppsett av MVC prosjekt

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TSD2491_oblig1_031688.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
